fb1b78149870e4490c25e88d08b508e765da7b75
Daniel Lukas <dtlukasart@gmail.com>
2021-10-30 09:42:06 -0400

    refactored comments

--- src/pomodoro/StopBtn.js
+++ src/pomodoro/StopBtn.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export default function StopBtn({ session, handleStop }){
+  // TODO: Implement stopping the current focus or break session. and disable the stop button when there is no active session
+  // TODO: Disable the stop button when there is no active session 
     return (
         <>
-        {/* TODO: Implement stopping the current focus or break session. and disable the stop button when there is no active session */}
-            {/* TODO: Disable the stop button when there is no active session */}
             <button
               type="button"
               className="btn btn-secondary"
